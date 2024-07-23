5838974d78affcf1d5acfb8aa5ba4a1f82f458c9
Hunter Koppen <hunter.koppen@kobeon.nl>
2024-07-23 23:07:08 +0200

    Update module for React Client

--- javasource/databasemonitor/actions/JA_ExecuteSQL.java
+++ javasource/databasemonitor/actions/JA_ExecuteSQL.java
@@ -7,7 +7,7 @@
 // Other code you write will be lost the next time you deploy the project.
 // Special characters, e.g., é, ö, à, etc. are supported in comments.
 
-package dbsizechecker.actions;
+package databasemonitor.actions;
 
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
